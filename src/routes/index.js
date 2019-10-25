ddde3cf10fa191c2557605cb9082049942e56656
GermanCode <w.aguirre@udla.edu.co>
2019-10-24 22:47:42 -0500

    todo

--- src/routes/index.js
+++ src/routes/index.js
@@ -13,9 +13,10 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', async (req, res)=>{
-    let err = 0.00001;
+    var cont = 0;
+    const err = 0.0000001;
 //Recibimos la funcion ingresada y la almacenamos en una vairable llamada "funcion"
-    const { funcion, x_inicial, y_inicial } = req.body;
+    var { funcion, x_inicial, y_inicial } = req.body;
 
     var x_i = nerdamer(x_inicial);
     var y_i = nerdamer(y_inicial);
@@ -45,38 +46,61 @@ fx1 = fx1.evaluate({y: y_i});
 var fy1 = derivY.evaluate({y: y_i});
 fy1 = fy1.evaluate({x: x_i});
 
-//Multiplicamos por t.
+while(Math.abs(fx1) > err || Math.abs(fy1) > err){
+
+    fx1 = derivX.evaluate({x: x_i});
+    fx1 = fx1.evaluate({y: y_i});
+    fy1 = derivY.evaluate({y: y_i});
+    fy1 = fy1.evaluate({x: x_i});
+    
+    //Multiplicamos por t.
 var fx1_t = t.multiply(fx1);
 var fy1_t = t.multiply(fy1);
+var cont = 0;
 
 //Agregamos el vector inicial.
-var fx1_t = x_i.add(fx1_t);
-var fy1_t = y_i.add(fy1_t);
-
-console.log(fx1_t.text());
-console.log(fy1_t.text());
-
+fx1_t = x_i.add(fx1_t);
+fy1_t = y_i.add(fy1_t);
 
 var e = nerdamer(func, {x: fx1_t, y: fy1_t});
 e = nerdamer('simplify(' + e +')');
 
 //Derivamos con respecto a t
 var derivT = nerdamer.diff(e, 't');
-console.log(derivT.text());
 var sol = nerdamer.solve(derivT, 't');
-console.log(sol.text());
 
-//while(Math.abs(fx1) > err || Math.abs(fy1) > err){
-    var g = func.evaluate({x: ifxt}, {y: ifyt});
-    //console.log(g.text());
-//}
+x_i = x_i.add(sol.multiply(fx1));
+y_i = y_i.add(sol.multiply(fy1));
+
+var x_iR = '' + x_i;
+var y_iR = '' + y_i;
+
+var resultX = x_iR.replace(/[[\]]/g,'')
+var rx = nerdamer('simplify(' + resultX +')');
+var resultY = y_iR.replace(/[[\]]/g,'')
+var ry = nerdamer('simplify(' + resultY +')');
+
+x_i = rx;
+y_i = ry;
+
+x_inicial = x_i;
+y_inicial = y_i;
+
+cont +=  1;
+
+console.log(x_i.text());
+console.log(y_i.text());
+console.log('x_inicial', x_inicial.text());
+console.log(cont);
+
+}
 
 // Agregamos la funcion a la bd
-    const newFunc = {
-        funcion
-    };
-    console.log(newFunc);
-    await pool.query('INSERT INTO funcionprueba SET ?', [newFunc]);
+    // const newFunc = {
+    //     funcion
+    // };
+    // console.log(newFunc);
+    // await pool.query('INSERT INTO funcionprueba SET ?', [newFunc]);
     res.render('index', {data: funcion,
     visible: 'none',
     visible2: 'block',
