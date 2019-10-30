e3a8db7e753d5ff397e4659f706a6c2803215a1e
GermanCode <w.aguirre@udla.edu.co>
2019-10-29 22:42:07 -0500

    funciona!

--- src/keys.js
+++ src/keys.js
@@ -1,10 +1,10 @@
 module.exports = {
 
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
-        database: 'gradientapp'
+        host: 'us-cdbr-gcp-east-01.cleardb.net',
+        user: 'b4d5484c5fca11',
+        password: 'aaf2c9bf',
+        database: 'gcp_95ac59003c9eca132054'
     }
 
 };
