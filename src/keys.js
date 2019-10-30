586848dc50aa1897e04a6a4c15cab3a138c90cdc
GermanCode <w.aguirre@udla.edu.co>
2019-10-29 23:33:34 -0500

    Funciona local

--- src/keys.js
+++ src/keys.js
@@ -1,10 +1,16 @@
 module.exports = {
 
     database: {
-        host: 'us-cdbr-gcp-east-01.cleardb.net',
-        user: 'b4d5484c5fca11',
-        password: 'aaf2c9bf',
-        database: 'gcp_95ac59003c9eca132054'
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'gradientapp'
     }
+    // database: {
+    //     host: 'us-cdbr-gcp-east-01.cleardb.net',
+    //     user: 'b4d5484c5fca11',
+    //     password: 'aaf2c9bf',
+    //     database: 'gcp_95ac59003c9eca132054'
+    // }
 
 };
