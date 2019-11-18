266ebeacf747bf9a59c898bdcf08ebdf63a87691
GermanCode <w.aguirre@udla.edu.co>
2019-11-18 12:39:02 -0500

    con bd para validaciones

--- src/keys.js
+++ src/keys.js
@@ -1,17 +1,17 @@
 module.exports = {
     //Desarrollo
-    database: {
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
-        database: 'gradientapp'
-    }
-
-    //Producción
     // database: {
-    //     host: 'nt71li6axbkq1q6a.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    //     user: 'j4udxeq2g7okopn2',
-    //     password: 'ymdbvdfp4e08jdsy',
-    //     database: 'in2chpypqy4uxd2y'
+    //     host: 'localhost',
+    //     user: 'root',
+    //     password: 'root',
+    //     database: 'gradientapp'
     // }
+
+    Producción
+    database: {
+        host: 'nt71li6axbkq1q6a.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'j4udxeq2g7okopn2',
+        password: 'ymdbvdfp4e08jdsy',
+        database: 'in2chpypqy4uxd2y'
+    }
 };
